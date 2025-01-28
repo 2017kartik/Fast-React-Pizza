df21319038036e07adf532441530d42b5a849515
2017kartik <kartikagarwal9119063420@gmail.com>
2025-01-28 10:13:46 +0530

    Modeling the 'User' state with Redux Toolkit.

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,3 +1,9 @@
+import { useSelector } from "react-redux";
+
 export default function Username() {
-  return <div className="text-sm font-semibold hidden md:block">Kartik</div>;
+  const username = useSelector((state) => state.user.username);
+  if (!username) return null;
+  return (
+    <div className="text-sm font-semibold hidden md:block">{username}</div>
+  );
 }
